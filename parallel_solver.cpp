97d18fd56c51de2aa54447e903f4917c7c602abb
Antonyjuan Miranda Yquira <antonyjuan.miranda@ucsp.edu.pe>
2021-11-24 11:55:41 -0500

    Added the threading part

--- parallel_solver.cpp
+++ parallel_solver.cpp
@@ -4,9 +4,22 @@
 #include <vector>
 #include <filesystem>
 #include <thread>
+#include <string>
 
 using namespace std;
 namespace fs = filesystem;
+
+string int_to_string(int number, int digits)
+{
+    string ans = "";
+    while (number)
+    {
+        ans.push_back('0' + (number % 10));
+        number /= 10;
+    }
+    reverse(ans.begin(), ans.end());
+    return string(digits - ans.size(), '0') + ans;
+}
 
 void read_CSV(string argv, vector<string>* vectorX, vector<string>* vectorY){
     ifstream file(argv);
@@ -30,8 +43,8 @@ void read_CSV(string argv, vector<string>* vectorX, vector<string>* vectorY){
     }
 }
 
-void write_TSP(vector<string> vectorX, vector<string> vectorY){
-    string filename("LKH-2.0.9/cities.tsp");
+void write_TSP(vector<string> vectorX, vector<string> vectorY, string index){
+    string filename = "LKH-2.0.9/cities" + index + ".tsp";
     fstream outfile;
 
     outfile.open(filename, std::ios_base::out);
@@ -52,16 +65,16 @@ void write_TSP(vector<string> vectorX, vector<string> vectorY){
     }
 }
 
-void write_parameters(){
-    string filename("LKH-2.0.9/params.par");
+void write_parameters(string index){
+    string filename = "LKH-2.0.9/params" + index + ".par";
     fstream outfile;
 
     outfile.open(filename, std::ios_base::out);
     if (!outfile.is_open()) {
         cout << "failed to open " << filename << '\n';
     } else {
-        outfile << "PROBLEM_FILE = cities.tsp" << endl;
-        outfile << "OUTPUT_TOUR_FILE = tsp_solution.csv" << endl;
+        outfile << "PROBLEM_FILE = cities" << index << ".tsp" << endl;
+        outfile << "OUTPUT_TOUR_FILE = tsp_solution" << index << ".csv" << endl;
         outfile << "SEED = 2018" << endl;
         outfile << "CANDIDATE_SET_TYPE = POPMUSIC" << endl;
         outfile << "INITIAL_PERIOD = 10000" << endl;
@@ -82,15 +95,41 @@ double score_tour(string filename){
     return stod(score_value);
 }
 
-void solver(string filename){
+//void solver(string filename, string index, double* score){
+void solver(string filename, string index){
     vector<string> x;
     vector<string> y;
     read_CSV(filename, &x, &y);
-    write_TSP(x,y);
-    write_parameters();
-    system("cd .\\LKH-2.0.9 & .\\LKH params.par");
-    double score = score_tour("LKH-2.0.9/tsp_solution.csv");
+    write_TSP(x, y, index);
+    write_parameters(index);
+    string command = "cd .\\LKH-2.0.9 & .\\LKH params" + index + ".par & cls";
+    const char* _command = command.c_str();
+    system(_command);
+    string file_name = "LKH-2.0.9/tsp_solution" + index + ".csv";
+    //*score = score_tour(file_name);
+    double score = score_tour(file_name);
     cout << "--------------------------------" << score << endl;
+}
+
+double parallel_solver(vector<string> paths){
+    vector<thread> threadVect;
+    double* scores = new double [paths.size()];
+	//unsigned int threadSpread = N / numThreads;
+	for (unsigned int i = 0; i < paths.size(); i++) {
+        string index = int_to_string(i, 3);
+		//threadVect.emplace_back(solver, paths[i], index, scores[i]);
+        threadVect.emplace_back(solver, paths[i], index);
+	}
+	for (auto& t : threadVect) {
+		t.join();
+	}
+    /*for(unsigned int i = 0; i < paths.size(); i++){
+        cout << scores[i] << endl;
+    }*/
+    // Find the smallest score
+    //
+    delete[] scores;
+    return 0;
 }
 
 int main(){
@@ -102,6 +141,17 @@ int main(){
         string path_string = entry.path().string();
         paths.push_back(path_string);
     }
-    solver(paths[0]);
+
+    // Parallel part
+    unsigned int number_threads = thread::hardware_concurrency();
+    cout << "Number of threads: " << number_threads << endl;
+    double final_score;
+    //T0 = clock();
+    final_score = parallel_solver(paths);
+	//T1 = clock();
+
+    //double time = (double(T1 - T0) / CLOCKS_PER_SEC);
+	//cout << "Execution time: " << time << endl;
+
     return 0;
 }
